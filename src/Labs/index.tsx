7d18decdb6813ffb6695dbdd55b7c68c161f755d
Gargi Umrajkar <gargiumrajkar@Gargis-MacBook-Air.local>
2024-05-21 18:27:10 -0400

    Added Section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Gargi Girish Umrajkar</h1>
+      <h1>Gargi Girish Umrajkar Section 2</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
