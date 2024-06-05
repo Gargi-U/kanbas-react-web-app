1d96224db8c1fd1287c8d269999e9cd3ec957623
Gargi Umrajkar <gargiumrajkar@Gargis-MacBook-Air.local>
2024-06-05 03:07:11 -0400

    added checkmarks and bootstrap elements to the modules section

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,84 +1,101 @@
+import React from 'react';
+import ModulesControls from "./ModulesControls";
+import LessonControlButtons from "./LessonControlButtons";
+import { BsGripVertical } from "react-icons/bs";
+import ModuleControlButtons from "./ModuleControlButtons";
+
 export default function Modules() {
-    return (
-      <div id="wd-modules-container">
-        <div id="wd-modules-controls">
-          <button>Collapse All</button>
-          <button>View Progress</button>
-          <select className="wd-publish-options">
-            <option value="publish-all">Publish All</option>
-            <option value="publish-modules-only">Publish modules only</option>
-            <option value="publish-all-items">Publish all modules and items</option>
-            <option value="unpublish-modules-only">Unpublish modules only</option>
-            <option value="unpublish-all-items">Unpublish all modules and items</option>
-          </select>
-          <button>+ Module</button>
-        </div>
-  
-        <div id="wd-modules">
-          <ul>
-            <li className="wd-module">
-              <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
-              <ul className="wd-lessons">
-                <li className="wd-lesson">
-                  <strong>LEARNING OBJECTIVES</strong>
-                  <ul>
-                    <li>Introduction to the course</li>
-                    <li>Learn what is Web Development</li>
-                  </ul>
-                </li>
-                <li className="wd-lesson">
-                  <strong>READING</strong>
-                  <ul>
-                    <li>Full Stack Developer - Chapter 1 - Introduction</li>
-                    <li>Full Stack Developer - Chapter 2 - Creating User Interfaces</li>
-                  </ul>
-                </li>
-                <li className="wd-lesson">
-                  <strong>SLIDES</strong>
-                  <ul>
-                    <li>Introduction to Web Development</li>
-                    <li>Creating an HTTP server with Node.js</li>
-                    <li>Creating a React Application</li>
-                  </ul>
-                </li>
-              </ul>
+  return (
+    <div id="wd-modules">
+      <ModulesControls /><br /><br /><br /><br />
+      <ul id="wd-modules" className="list-group rounded-0">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            Week 1
+            <ModuleControlButtons />
+          </div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <strong data-toggle="collapse" data-target="#learningObjectives1">
+                <BsGripVertical className="me-2 fs-3" />
+                LEARNING OBJECTIVES
+                <LessonControlButtons />
+              </strong>
+              <div id="learningObjectives1" className="collapse">
+                <ul>
+                  <li>Introduction to the course</li>
+                  <li>Learn what is Web Development</li>
+                </ul>
+              </div>
             </li>
-  
-            <li className="wd-module">
-              <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
-              <ul className="wd-lessons">
-                <li className="wd-lesson">
-                  <strong>LEARNING OBJECTIVES</strong>
-                  <ul>
-                    <li>Learn how to create user interfaces with HTML</li>
-                    <li>Deploy the assignment to Netlify</li>
-                  </ul>
-                </li>
-                <li className="wd-lesson">
-                  <strong>SLIDES</strong>
-                  <ul>
-                    <li>Introduction to HTML and the DOM</li>
-                    <li>Formatting Web content with Headings and Titles</li>
-                    <li>Formatting content with Lists and Tables</li>
-                  </ul>
-                </li>
-              </ul>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <strong data-toggle="collapse" data-target="#reading1">
+                <BsGripVertical className="me-2 fs-3" />
+                READING
+                <LessonControlButtons />
+              </strong>
+              <div id="reading1" className="collapse">
+                <ul>
+                  <li>Full Stack Developer - Chapter 1 - Introduction</li>
+                  <li>Full Stack Developer - Chapter 2 - Creating User Interfaces</li>
+                </ul>
+              </div>
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <strong data-toggle="collapse" data-target="#slides1">
+                <BsGripVertical className="me-2 fs-3" />
+                SLIDES
+                <LessonControlButtons />
+              </strong>
+              <div id="slides1" className="collapse">
+                <ul>
+                  <li>Introduction to Web Development</li>
+                  <li>Creating an HTTP server with Node.js</li>
+                  <li>Creating a React Application</li>
+                </ul>
+              </div>
             </li>
           </ul>
-        </div>
-  
-        <div id="wd-course-status">
-          <div><strong>Course Status</strong></div>
-          <button>Unpublish</button>
-          <button>Publish</button>
-          <button>Import Existing Content</button>
-          <button>Import from Commons</button>
-          <button>Choose Home Page</button>
-          <button>View Course Stream</button>
-          <button>New Announcement</button>
-          <button>New Analytics</button>
-          <button>View Course Notifications</button>
-        </div>
-      </div>
-    );
-  }
+        </li>
+
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            Week 2
+            <ModuleControlButtons />
+          </div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <strong data-toggle="collapse" data-target="#learningObjectives2">
+                <BsGripVertical className="me-2 fs-3" />
+                LEARNING OBJECTIVES
+                <LessonControlButtons />
+              </strong>
+              <div id="learningObjectives2" className="collapse">
+                <ul>
+                  <li>Learn how to create user interfaces with HTML</li>
+                  <li>Deploy the assignment to Netlify</li>
+                </ul>
+              </div>
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <strong data-toggle="collapse" data-target="#slides2">
+                <BsGripVertical className="me-2 fs-3" />
+                SLIDES
+                <LessonControlButtons />
+              </strong>
+              <div id="slides2" className="collapse">
+                <ul>
+                  <li>Introduction to HTML and the DOM</li>
+                  <li>Formatting Web content with Headings and Titles</li>
+                  <li>Formatting content with Lists and Tables</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
